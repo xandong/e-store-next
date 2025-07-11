78174eadff8f952a9c8682bdcec455ec891ce417
Alexandre <xandongurgel@gmail.com>
2025-07-11 00:43:44 -0300

    fix: may be fixes prisma binary issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,8 @@
-import { PrismaClient } from "@/types/prisma/generated"
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client"
 
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export let prisma: PrismaClient
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
-  }
-
-  prisma = global.cachedPrisma
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
