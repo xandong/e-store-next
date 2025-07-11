eb85ffe71038cb3f41e64bde14041f7f9ac51835
Alexandre <xandongurgel@gmail.com>
2025-07-11 01:06:05 -0300

    chore: added prisma plugin as workaround to next

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, options) => {
+    if (options.isServer) {
+      config.plugins.push(new PrismaPlugin())
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
