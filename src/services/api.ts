79b25642ac6ced1c539c2e3eb7291f99c9d45905
Alexandre <xandongurgel@gmail.com>
2025-07-11 00:18:56 -0300

    fix: remove api type references

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,17 @@
-import {
-  Configuration,
-  AuthenticationApi,
-  UsersApi,
-  CategoriesApi,
-  ProductsApi
-} from "@/types/api/generated"
+// import {
+//   Configuration,
+//   AuthenticationApi,
+//   UsersApi,
+//   CategoriesApi,
+//   ProductsApi
+// } from "@/types/api/generated"
 
-export const config: Configuration = {
-  basePath: process.env.NEXT_PUBLIC_API_BASE_URL,
-  isJsonMime: () => true
-}
+// export const config: Configuration = {
+//   basePath: process.env.NEXT_PUBLIC_API_BASE_URL,
+//   isJsonMime: () => true
+// }
 
-export const authenticationApi = new AuthenticationApi(config)
-export const usersApi = new UsersApi(config)
-export const categoriesApi = new CategoriesApi(config)
-export const productsApi = new ProductsApi(config)
+// export const authenticationApi = new AuthenticationApi(config)
+// export const usersApi = new UsersApi(config)
+// export const categoriesApi = new CategoriesApi(config)
+// export const productsApi = new ProductsApi(config)
